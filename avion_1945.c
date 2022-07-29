72101b76d674453a8eb2f459ba2eadabff65b690
Matias <montecinosmatias142@gmail.com>
2022-07-28 21:22:47 -0300

    fase 3.3

--- avion_1945.c
+++ avion_1945.c
@@ -4,6 +4,8 @@
 
 #include<stdlib.h>
 
+#include<time.h>
+
 #include "bala.h"
 
 #define MAXCOL 15
@@ -16,8 +18,9 @@ BITMAP *buffer;
 void init();
 
 void deinit();
-	
+
 char escena[MAXFIL][MAXCOL];
+
 
 void leer()//codigo de ayuda por mi compañero francisco
 {
@@ -70,7 +73,7 @@ void dibujar()
 	{
 		for(j=0;j<MAXCOL;j++)
 		{
-			if(escena[i][j] == 'X')
+			if(escena[i][j] == 'X' )
 			{
 				draw_sprite(screen,pared,20*j,15*i);
 			}
@@ -87,8 +90,10 @@ int main()
 	int y1=155;
 	int x2=200;
 	int y2=450;
-			
-	BITMAP *buffer = create_bitmap(440,580);
+	int mov=200;
+	int mos=450;
+	
+	BITMAP *buffer = create_bitmap(440,680);
 	//se declara el bitmap para poder insertar la imagen del avion que usara el usuario
 	
 	BITMAP *avion;
@@ -107,29 +112,30 @@ int main()
 
 	set_window_title("1945");
 	
-	BITMAP *disparr;
+	BITMAP *enemigo;
+		
+	enemigo = load_bitmap("enemigo.bmp",NULL);
 	
-	disparr = load_bitmap("lal.bmp",NULL);
-			
+
 	while (!key[KEY_ESC]) // ciclo que ayuda amover el avion y deja la imagen de fondo 
-	{	
+	{
 		blit(fondo,screen,0,0,x1,y1,440,680);
 		
 		blit(fondo,buffer,0,0,0,0,fondo->w,fondo->h);//deja el fondo estatico y deja sobre poner el personaje 
-				
+			
 		//avion
-		blit(buffer,screen,0,0,0,0,700,600);
+		blit(buffer,screen,0,0,0,0,440,680);
 		
 		blit(avion,screen,0,0,x,y,avion->w,avion->h);
 
 		//blit(disparr,fondo,0,0,x1,y1,disparr->w,disparr->h);		
-		
-		dibujar();//funcion que sirve para leer y ejecutar un archivo de txt
+			
+		blit(enemigo,screen,0,0,0,0,enemigo->w,enemigo->h);
 		
 		if(key[KEY_SPACE])
 		{
 			y2-=6;//esto hace que el disparo ocurra hacia adelante y a su vez hace que aparezca la bala 
-			blit(buffer,screen,0,0,0,0,700,600);
+			blit(buffer,screen,0,0,0,0,440,680);
 			blit(sprite,screen,0,0,x2,y2,sprite->w,sprite->h);
 		}
 		else if(key[KEY_RIGHT])//teclas de movimiento del avion 
@@ -141,46 +147,49 @@ int main()
 		else if(key[KEY_LEFT])
 		{
 			x-=50;//hace que se mueva hacia la izquierda
-			blit(buffer,screen,0,0,0,0,700,600);
+			blit(buffer,screen,0,0,0,0,440,680);
 			blit(avion,screen,0,0,x,y,100,112);
 		}
 		else if(key[KEY_UP])
 		{
 			y-=50;//hace que se mueva hacia arriba , el signo menos es porque allegro tiene invertido las direcciones de arriba y abajo 
-			blit(buffer,screen,0,0,0,0,700,600);
+			blit(buffer,screen,0,0,0,0,440,680);
 			blit(avion,screen,0,0,x,y,100,112);
 		}
 		else if(key[KEY_DOWN])
 		{
 			y+=50;//se mueve hacia abajo
-			blit(buffer,screen,0,0,0,0,700,600);
+			blit(buffer,screen,0,0,0,0,440,680);
 			blit(avion,screen,0,0,x,y,100,112);
 		}
 		// "botones de reserva"
 		if(key[KEY_D])
 		{
 			x+=50;
-			blit(buffer,screen,0,0,0,0,700,600);
+			blit(buffer,screen,0,0,0,0,440,680);
 			blit(avion,screen,0,0,x,y,100,112);
 		}
 		else if(key[KEY_A])
 		{
 			x-=50;
-			blit(buffer,screen,0,0,0,0,700,600);
+			blit(buffer,screen,0,0,0,0,440,680);
 			blit(avion,screen,0,0,x,y,100,112);
 		}
 		else if(key[KEY_W])
 		{
 			y-=50;
-			blit(buffer,screen,0,0,0,0,700,600);
+			blit(buffer,screen,0,0,0,0,440,680);
 			blit(avion,screen,0,0,x,y,100,112);
 		}
 		else if(key[KEY_S])
 		{
 			y+=50;
-			blit(buffer,screen,0,0,0,0,700,600);
+			blit(buffer,screen,0,0,0,0,440,680);
 			blit(avion,screen,0,0,x,y,100,112);
 		}
+		
+		dibujar();//funcion que sirve para leer y ejecutar un archivo de txt
+
 		rest(10);
 	}
 	deinit();
